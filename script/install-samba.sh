f3ff615cadf889aa04da372dd4ca5b1d919644ad
hallatech <warren@spindriftgroup.com>
2017-07-25 21:43:18 +0900

    Fix samba configuration for shared reverse dirs

--- script/install-samba.sh
+++ script/install-samba.sh
@@ -6,4 +6,14 @@ if [[ "$INSTALL_SAMBA" == *true* ]]; then
 
   # For Windows SMB install cifs-utils
   yum -y install cifs-utils
+  
+  #Set samba to start on boot
+  sudo /sbin/chkconfig --level 345 smb on
+    
+  #Set password for vagrant
+  (echo "vagrant";echo "vagrant") | sudo smbpasswd -a vagrant
+  
+  #Configure samba
+  cp /tmp/smb.conf /etc/samba
+  rm /tmp/smb.conf
 fi
